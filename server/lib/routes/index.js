36b3ffa1f2ff2aa612aa07816e5f32848846caa0
thanhhoa1906 <vudithanh@gmail.com>
2022-04-04 18:26:59 +0700

    Delete index.js

--- server/lib/routes/index.js
+++
@@ -1,7 +0,0 @@
-// Load the page
-// NB: This needs to be the last route added
-exports.serveIndex = function (app, staticFolder) {
-  app.get('*', function (req, res) {
-    res.sendFile('index.html', { root: staticFolder });
-  });
-};
